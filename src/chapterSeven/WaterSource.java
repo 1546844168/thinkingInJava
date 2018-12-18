4c7e923eda2fa2d8c06a0c626a3d1e7b39d9a218
feng.jiang <feng.jiang@irootech.com>
2018-12-18 21:45:09 +0800

    再次学习 thinking in java

---
+++ src/chapterSeven/WaterSource.java
@@ -0,0 +1,21 @@
+package chapterSeven;
+
+/**
+ * @author 江峰
+ * @create 2018-12-18 20:40
+ */
+public class WaterSource {
+    private String s;
+
+    public WaterSource() {
+        System.out.println("水源");
+        s = "constructeed";
+    }
+
+    @Override
+    public String toString() {
+        return "WaterSource{" +
+                "s='" + s + '\'' +
+                '}';
+    }
+}
